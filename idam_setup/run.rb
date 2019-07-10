310fd8f2333d35c9d2461a04bffa5879bc8c6f7c
Gary Taylor <gary.taylor@hismessages.com>
2019-07-10 15:56:55 +0100

    Employment Tribunal specific stuff

--- idam_setup/run.rb
+++ idam_setup/run.rb
@@ -48,15 +48,35 @@ session.click_link "Manage roles"
 session.choose('CCD Gateway As Instructed In ccd-docker (ccd_gateway)', visible: false)
 session.click_button 'Continue'
 
-
-['ccd-import', 'caseworker', 'caseworker-test', 'caseworker-employment-tribunal-manchester', 'caseworker-employment', 'caseworker-employment-tribunal-manchester-caseofficer', 'caseworker-publiclaw-localAuthority'].each do |role|
+add_role = ->(role, selecting: []) do
   next if session.has_selector?(:radio_button, role, visible: false)
   session.choose "Add a new role", visible: false
   session.click_button "Continue"
   session.fill_in "Role label", with: role
   session.fill_in "Description", with: role
+  selecting.each do |role|
+    session.check role, visible: false, name: 'assignableRoles'
+  end
   session.click_button "Save this role"
-end
 
+end
+add_role.call('ccd-import')
+puts `../bin/idam-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker`
+add_role.call('caseworker')
+add_role.call('caseworker-employment', selecting: ['caseworker'])
+add_role.call('caseworker-employment-tribunal-glasgow', selecting: ['caseworker', 'caseworker-employment'])
+add_role.call('caseworker-employment-tribunal-manchester', selecting: ['caseworker', 'caseworker-employment'])
+add_role.call('caseworker-employment-tribunal-glasgow-caseofficer', selecting: ['caseworker', 'caseworker-employment', 'caseworker-employment-tribunal-glasgow'])
+add_role.call('caseworker-employment-tribunal-manchester-caseofficer', selecting: ['caseworker', 'caseworker-employment', 'caseworker-employment-tribunal-manchester'])
+add_role.call('caseworker-employment-tribunal-glasgow-casesupervisor', selecting: ['caseworker', 'caseworker-employment', 'caseworker-employment-tribunal-glasgow'])
+add_role.call('caseworker-employment-tribunal-manchester-casesupervisor', selecting: ['caseworker', 'caseworker-employment', 'caseworker-employment-tribunal-manchester'])
+puts `../bin/ccd-add-role.sh caseworker-employment`
+puts `../bin/ccd-add-role.sh caseworker-employment-tribunal-manchester-caseofficer`
+puts `../bin/ccd-add-role.sh caseworker-employment-tribunal-manchester-casesupervisor`
+puts `../bin/ccd-add-role.sh caseworker-employment-tribunal-manchester`
+puts `../bin/ccd-add-role.sh caseworker-employment-tribunal-glasgow-caseofficer`
+puts `../bin/ccd-add-role.sh caseworker-employment-tribunal-glasgow-casesupervisor`
+puts `../bin/ccd-add-role.sh caseworker-employment-tribunal-glasgow`
+puts `../bin/idam-create-caseworker.sh caseworker,caseworker-employment-tribunal-manchester-caseofficer,caseworker-employment-tribunal-manchester-casesupervisor,caseworker-employment,caseworker-employment-tribunal-manchester,caseworker-employment-tribunal-glasgow-caseofficer,caseworker-employment-tribunal-glasgow-casesupervisor,caseworker-employment-tribunal-glasgow m@m.com Pa55word11 Lightyear Buzz`
 
 sleep 10
